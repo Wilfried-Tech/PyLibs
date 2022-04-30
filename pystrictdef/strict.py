316a0592252d7a1b41be93e57d07f1b131b4ece6
Wilfried-Tech <wilfriedlarry63@gmail.com>
2022-04-30 15:56:16 +0100

    Committed/ by bash script

--- pystrictdef/strict.py
+++ pystrictdef/strict.py
@@ -1,11 +1,14 @@
 import types
 
 def checkType(*t_args, **t_kwargs):
-  
   def decorator(function):
     def checker(*args, **kwargs):
-      pass
-    
+      if len(args) != len(t_args):
+        raise TypeError("{0.__name__}() missing {diff} required positional argument".format(function, diff=len(t_args)-len(args)))
+      for i,_type in enumerate(t_args):
+        if type(args[i]) is not _type:
+          raise TypeError("the argument {index} must be {type} type".format(index=i, type= _type.__name__))
+      return function(*args, **kwargs)
     return checker
   return decorator
 
--- test-main.py
+++ test-main.py
@@ -2,8 +2,10 @@ from pystrictdef.strict import checkType
 
 import pystrictdef.strict as strict
 
-@checkType()
-def test():
-  pass
+@checkType(int,int)
+def mul(num,l=10):
+  for i in range(1,l+1): 
+    print(i,"x",num,"=",i*num)
+  return ''
 
-print(test())
+print(mul(5,6))
