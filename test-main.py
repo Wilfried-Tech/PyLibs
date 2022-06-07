982ccc4c05b0e574104f4ef84e43a1daf09f54bb
Wilfried-Tech <wilfriedlarry63@gmail.com>
2022-06-07 16:21:17 +0100

    Committed/ by bash script

--- test-main.py
+++ test-main.py
@@ -1,31 +1,40 @@
-import socket
+# import socket
 
-from wsocket._wsocket import WSocket
+# from wsocket._wsocket import WSocket
 
-s = input("serveur ou client")
-address = 'localhost'
-port = 5000
+# s = input("serveur ou client")
+# address = 'localhost'
+# port = 5000
 
-conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-def scmsg(m):
-    print("onmessage sclient", m.decode("utf8"))
+# conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-def cmsg(m):
-    print("onmessage client", m.decode("utf8"))
+# def scmsg(m):
+#     print("onmessage sclient", m.decode("utf8"))
 
 
-if int(s) == 1:
-    conn.bind((address, port))
-    conn.listen(1)
-    client, address = conn.accept()
-    wclient = WSocket(client)
-    wclient.send("it work")
-    wclient.onmessage(scmsg)
-else:
-    conn.connect((address, port))
-    wclient = WSocket(conn)
-    wclient.onmessage(scmsg)
-    wclient.send("it work")
+# def cmsg(m):
+#     print("onmessage client", m.decode("utf8"))
+
+
+# if int(s) == 1:
+#     conn.bind((address, port))
+#     conn.listen(1)
+#     client, address = conn.accept()
+#     wclient = WSocket(client)
+#     wclient.send("it work")
+#     wclient.onmessage(scmsg)
+# else:
+#     conn.connect((address, port))
+#     wclient = WSocket(conn)
+#     wclient.onmessage(scmsg)
+#     wclient.send("it work")
+
+from pystrictdef.strict import checkType
+
+
+@checkType(int, int)
+def test(a, b):
+    pass
+
+test("",5)
